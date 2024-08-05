27408882e0d24210558cd033fa6b59d95f3d9f8c
caiya <admin@yaohst.com>
2024-08-05 11:30:41 +0800

    日志路径变更

--- gateway.sh
+++ gateway.sh
@@ -4,4 +4,4 @@ sleep 60s
 #启动进程
 JAVA_OPT="-Xms256m -Xmx512m --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.math=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.util.concurrent=ALL-UNNAMED --add-opens java.rmi/sun.rmi.transport=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED"
 
-nohup /opt/jdk17/bin/java  -jar $JAVA_OPT /app/java/smart-gateway.jar >/app/java/smart-gateway`date +%Y%m%d`.log 2>&1 &
+nohup /opt/jdk17/bin/java  -jar $JAVA_OPT /app/java/smart-gateway.jar >/app/java/logs/smart-gateway`date +%Y%m%d`.log 2>&1 &
